e4e09d1d945c6b6557b673ddd29c7c92c6337696
Mattia Roccoberton <mat@blocknot.es>
2021-05-27 09:02:09 +0200

    Webpack support

---
+++ index.js
@@ -0,0 +1,6 @@
+// Styles
+import 'activeadmin_froala_editor/app/assets/stylesheets/activeadmin/_froala_editor_input'
+
+// JS
+window.FroalaEditor = require('activeadmin_froala_editor/app/assets/javascripts/activeadmin/froala_editor/froala_editor.pkgd.min')
+require('activeadmin_froala_editor/app/assets/javascripts/activeadmin/froala_editor_input')
